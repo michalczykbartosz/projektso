a4e193ff4ef435a26880656d345f30aebf9f6965
bart3 <bart3q747@gmail.com>
2026-01-07 17:53:47 +0100

    Poprawa komentarzy w klasie kolejka

--- kolejka.cpp
+++ kolejka.cpp
@@ -12,13 +12,14 @@ kolejka::kolejka() //konstruktor - tworzenie klucza do kolejki komunikatow
 	key_t klucz = 33333;
 	//klucz = ftok("main.cpp", 'K');
 
-	pid_dyspozytora = getpid();
+	pid_dyspozytora = getpid(); //ustawienie pid_dyspozytora na proces tworzacy kolejke
 
-	id_kolejka = msgget(klucz, IPC_CREAT | 0600);
+	id_kolejka = msgget(klucz, IPC_CREAT | 0600); //tworzenie kolejki
 
+	//wyjscie jesli kolejka sie nie utworzyla
 	if (id_kolejka == -1)
 	{
-		perror("Blad przy tworzeniu kolejki komunikatow!");
+		perror("Blad przy tworzeniu kolejki komunikatow!"); //wypisanie bledu 
 		exit(1);
 	}
 
@@ -32,36 +33,39 @@ kolejka::~kolejka() //destruktor ktory automatycznie usuwa kolejke komunikatow j
 	}
 }
 
-void kolejka::wyslij(int typ,int id_nadawcy, const char* tekst) //funkcja dodajaca komuniakat do kolejki 
+//funkcja dodajaca komunikat do kolejki 
+void kolejka::wyslij(int typ,int id_nadawcy, const char* tekst) 
 {
 	struct komunikat msg;
 
-	msg.mtype = typ;
-	msg.id_nadawcy = id_nadawcy;
+	msg.mtype = typ; //ustawianie typu wiadomosci
+	msg.id_nadawcy = id_nadawcy; //ustawianie id_nadawcy
 
-	strncpy(msg.text, tekst, sizeof(msg.text) - 1);
-	msg.text[sizeof(msg.text) - 1] = '\0';
+	strncpy(msg.text, tekst, sizeof(msg.text) - 1); //kopiowanie tekstu do wiadomosci kolejki
+	msg.text[sizeof(msg.text) - 1] = '\0'; //upewnienie sie ze na koncu wiadomosci jest "\0"
 
 
-
+	//jesli nie udalo sie wyslac wiadomosci, wypisujemy blad (rozmiar to wielkosc struktury - pole mtype)
 	if (msgsnd(id_kolejka,&msg, sizeof(komunikat) - sizeof(long int), 0) == -1)
 	{
-		perror("Blad wysylania komunikatu!");
+		perror("Blad wysylania komunikatu!"); //wypisanie bledu
 	}
 }
 
-komunikat kolejka::odbierz(int typ) //funkcja pobierania komunikatu z kolejki
+//funkcja odbierajaca wiadomosc z kolejki
+komunikat kolejka::odbierz(int typ) 
 {
 	komunikat msg;
 
+	//jesli nie udalo sie odebrac wiadomosci, wypisujemy blad
 	if (msgrcv(id_kolejka, &msg, sizeof(msg) - sizeof(long), typ, 0) == -1)
 	{
-		perror("Blad odbierania komunikatu!");
+		perror("Blad odbierania komunikatu!"); //wypisanie bledu
 	}
 	return msg;
 }
-
-int kolejka::odbierz_nieblokujaco(int typ, komunikat& msg) //funkcja do sprawdzania czy w ogole jest jakis komunikat (dla ciezarowki) i nie blokuje
+//funkcja odbierania komunikatu z kolejki do sprawdzania czy jest pakiet ekspresowy, tak aby nie blokowalo pracy ciezarowki
+int kolejka::odbierz_nieblokujaco(int typ, komunikat& msg) 
 
 {
 	return msgrcv(id_kolejka, &msg, sizeof(msg) - sizeof(long), typ, IPC_NOWAIT);
--- kolejka.h
+++ kolejka.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
@@ -7,18 +7,20 @@
 class kolejka
 {
 private:
-	int id_kolejka;
-	int pid_dyspozytora;
+	int id_kolejka; //systemowe id kolejki komunikatow
+	int pid_dyspozytora; //pid procesu glownego, tylko proces z tym pidem moze wywolac destruktor
 
 public:
-	kolejka();
-	~kolejka();
+	kolejka(); //konstruktor tworzacy kolejke lub pobiera istniejaca
+	~kolejka(); //destruktor niszczacy kolejke jesli biezacy proces to pid_dyspozytora
 
-	void wyslij(int typ,int id_nadawcy,const char* tekst);
+	void wyslij(int typ,int id_nadawcy,const char* tekst); /*funkcja wysylajaca sformatowany komunikat do kolejki: typ - kanał(1: logi, 4 : ekspres)
+	tekst - tresc wiadomosci*/
 	
-	komunikat odbierz(int typ = 0);
+	komunikat odbierz(int typ = 0); //funkcja odbierajaca komunikat w trybie blokujacym (proces jest usypiany az nadejdzie wiadomosc)
 
-	int odbierz_nieblokujaco(int typ, komunikat& msg);
+	int odbierz_nieblokujaco(int typ, komunikat& msg); /*funkcja odbierajaca komunikat w trybie nieblokujacym uzywajaca flage IPC_NOWAIT
+	tak aby ciezarowki mogly sprawdzac czy jest pakiet ekspresowy bez zatrzymywania pracy*/
 
 	
 
